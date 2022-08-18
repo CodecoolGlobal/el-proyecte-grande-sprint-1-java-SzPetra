0f4122e2d2c02f2e97051d6867ae570f679b60a2
enwoy <androkityb@gmail.com>
2022-08-18 11:25:18 +0200

    refactor: StudentController with autowire

--- LnkdOut/src/main/java/application/controller/student/StudentController.java
+++ LnkdOut/src/main/java/application/controller/student/StudentController.java
@@ -2,7 +2,9 @@ package application.controller.student;
 
 import application.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class StudentController {
 
     private StudentService studentService;
