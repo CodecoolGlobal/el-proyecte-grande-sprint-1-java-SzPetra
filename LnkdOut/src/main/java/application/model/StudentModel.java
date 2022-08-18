172f286ec3169d975cd984adfeadd9e97b73a74e
enwoy <androkityb@gmail.com>
2022-08-18 13:37:13 +0200

    feat: add basemodel constructor and super to child

--- LnkdOut/src/main/java/application/model/StudentModel.java
+++ LnkdOut/src/main/java/application/model/StudentModel.java
@@ -1,4 +1,12 @@
 package application.model;
 
+import java.util.UUID;
+
 public class StudentModel extends BaseModel{
+
+    public StudentModel(String name, int age) {
+        super(name, age);
+    }
+
+
 }
