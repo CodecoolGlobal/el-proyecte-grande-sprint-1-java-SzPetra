92814187329f6fb1ab14a6855f0ed5c8a8b17e6c
enwoy <androkityb@gmail.com>
2022-08-18 13:39:07 +0200

    feat: create student example and saved in memory

--- LnkdOut/src/main/java/application/service/StudentService.java
+++ LnkdOut/src/main/java/application/service/StudentService.java
@@ -2,11 +2,9 @@ package application.service;
 
 import application.model.StudentModel;
 import application.service.dao.StudentDao;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
+
 
 public class StudentService {
 
