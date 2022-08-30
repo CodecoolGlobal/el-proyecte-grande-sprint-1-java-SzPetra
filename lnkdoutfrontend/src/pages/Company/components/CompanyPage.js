9b655a39fe3074ae9b1f04a84211035abacb9c32
enwoy <androkityb@gmail.com>
2022-08-30 13:06:21 +0200

    feat: add company registration form

--- lnkdoutfrontend/src/pages/Company/components/CompanyPage.js
+++ lnkdoutfrontend/src/pages/Company/components/CompanyPage.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Registration from "./Registration";
 
-function CompanyPage(props) {
+function CompanyPage() {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [city, setCity] = useState('');
+    const [isValid, setIsValid] = useState(false);
+
+    const validate = () => {
+        return password === confirmPassword;
+
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (validate()) {
+            setIsValid(false);
+            console.log(isValid);
+            setName('');
+            setEmail('');
+            setPhone('');
+            setPassword('');
+            setConfirmPassword('');
+            setCity('');
+        } else {
+            setIsValid(true);
+        }
+    }
+
     return (
         <div>
-
+        <Registration
+            name={name}
+            setName={setName}
+            email={email}
+            setEmail={setEmail}
+            phone={phone}
+            setPhone={setPhone}
+            password={password}
+            setPassword={setPassword}
+            confirmPassword={confirmPassword}
+            setConfirmPassword={setConfirmPassword}
+            city={city}
+            setCity={setCity}
+            handleSubmit={handleSubmit}
+            isValid={isValid}
+        />
         </div>
     );
 }
--- lnkdoutfrontend/src/pages/Company/components/Registration.js
+++ lnkdoutfrontend/src/pages/Company/components/Registration.js
@@ -1,11 +1,92 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function Registration(props) {
+function Registration({name, setName, email, setEmail, phone, setPhone,
+                          password, setPassword, confirmPassword, setConfirmPassword,
+                          city, setCity, handleSubmit, isValid}) {
     return (
-        <form>
-
+        <form className="form" onSubmit={handleSubmit}>
+            {isValid && <div>Password or Name is invalid</div>}
+            <div className="form-body">
+                <div className="username">
+                    <label className="form_label" htmlFor="name" >Name</label>
+                    <input
+                        required={true}
+                        className="form_input"
+                        type="text"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="Enter name here"
+                    />
+                </div>
+                <div className="email">
+                    <label className="form_label" htmlFor="email">Email</label>
+                    <input
+                        required={false}
+                        type="email"
+                        id="email"
+                        className="form_input"
+                        placeholder="Enter email here"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div className="password">
+                    <label className="form_label" htmlFor="password">Password </label>
+                    <input
+                        required={true}
+                        className="form_input"
+                        type="password"
+                        id="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <div className="confirm-password">
+                    <label className="form_label" htmlFor="confirmPassword">Confirm Password </label>
+                    <input
+                        required={true}
+                        className="form_input"
+                        type="password"
+                        id="confirmPassword"
+                        placeholder="Confirm Password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                </div>
+                <div className="phone">
+                    <label className="form_label" htmlFor="phone">Phone number</label>
+                    <input
+                        required={true}
+                        className="form_input"
+                        type="phone"
+                        id="phone"
+                        placeholder="Enter phone number"
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                    />
+                </div>
+                <div className="city">
+                    <label className="form_label" htmlFor="city">City</label>
+                    <input
+                        required={true}
+                        className="form_input"
+                        type="text"
+                        id="city"
+                        placeholder="Enter city here"
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)}
+                    />
+                </div>
+            </div>
+            <div className="footer">
+                <button
+                    type="submit"
+                    className="btn">Register</button>
+            </div>
         </form>
-    );
+    )
 }
 
 export default Registration;
