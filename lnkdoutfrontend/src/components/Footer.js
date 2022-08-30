feaa2117e66037d830ecb254bd19a62f0e0610cc
szgyuszi <szgybence@gmail.com>
2022-08-30 11:56:25 +0200

    feat: design footer layout

--- lnkdoutfrontend/src/components/Footer.js
+++ lnkdoutfrontend/src/components/Footer.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './assets/footer.css'
 
 function Footer(props) {
     return (
         <div className="footer">
-            {"Footer"}
+            <div className="footer-logo">
+                <p>{"Logo here"}</p>
+            </div>
+            <div className="footer-links">
+                <a href="/#">{"Cookie Policy"}</a>
+                <a href="/#">{"Privacy Policy"}</a>
+                <a href="/#">{"Site Map"}</a>
+            </div>
         </div>
     );
 }
