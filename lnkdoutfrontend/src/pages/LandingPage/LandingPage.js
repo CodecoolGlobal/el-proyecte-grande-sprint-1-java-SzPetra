a2b38eedc90c8ff84d2731ae6dc69039e4a6f00d
SzPetra <petra.szilagyi27@gmail.com>
2022-08-31 13:06:11 +0200

    feat: add registration button to landing page

--- lnkdoutfrontend/src/pages/LandingPage/LandingPage.js
+++ lnkdoutfrontend/src/pages/LandingPage/LandingPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Content from "./components/Content";
 // import Footer from "../../components/Footer";
+import GetStarted from "./components/GetStarted";
 
 function LandingPage(props) {
     return (
         <>
             <Content />
+            <GetStarted />
             {/*<Footer/>*/}
         </>
     );
