968d0fba20831efd437b65f1bfd4d2acb1a3fb98
SzPetra <petra.szilagyi27@gmail.com>
2022-09-01 11:40:13 +0200

    feat: add CSS to company registration page

--- lnkdoutfrontend/src/pages/Company/components/Registration.js
+++ lnkdoutfrontend/src/pages/Company/components/Registration.js
@@ -1,91 +1,95 @@
 import React, {useState} from 'react';
+import '../assets/company.css';
 
 function Registration({name, setName, email, setEmail, phone, setPhone,
                           password, setPassword, confirmPassword, setConfirmPassword,
                           city, setCity, handleSubmit, isValid}) {
     return (
-        <form className="form" onSubmit={handleSubmit}>
-            {isValid && <div>Password or Name is invalid</div>}
-            <div className="form-body">
-                <div className="username">
-                    <label className="form_label" htmlFor="name" >Name</label>
-                    <input
-                        required={true}
-                        className="form_input"
-                        type="text"
-                        id="name"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        placeholder="Enter name here"
-                    />
+        <div className={'company-container'}>
+            <img src={'https://englishclassviaskype.com/wp-content/uploads/2018/12/in-company-English-classes-head2.jpg'}/>
+            <form className="company-form" onSubmit={handleSubmit}>
+                {isValid && <div>Password or Name is invalid</div>}
+                <div className="form-body">
+                    <div className="username">
+                        <label className="form_label" htmlFor="name" >Name</label>
+                        <input
+                            required={true}
+                            className="form_input"
+                            type="text"
+                            id="name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            placeholder="Enter name here"
+                        />
+                    </div>
+                    <div className="email">
+                        <label className="form_label" htmlFor="email">Email</label>
+                        <input
+                            required={false}
+                            type="email"
+                            id="email"
+                            className="form_input"
+                            placeholder="Enter email here"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="password">
+                        <label className="form_label" htmlFor="password">Password </label>
+                        <input
+                            required={true}
+                            className="form_input"
+                            type="password"
+                            id="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className="confirm-password">
+                        <label className="form_label" htmlFor="confirmPassword">Confirm Password </label>
+                        <input
+                            required={true}
+                            className="form_input"
+                            type="password"
+                            id="confirmPassword"
+                            placeholder="Confirm Password"
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className="phone">
+                        <label className="form_label" htmlFor="phone">Phone number</label>
+                        <input
+                            required={true}
+                            className="form_input"
+                            type="phone"
+                            id="phone"
+                            placeholder="Enter phone number"
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                        />
+                    </div>
+                    <div className="city">
+                        <label className="form_label" htmlFor="city">City</label>
+                        <input
+                            required={true}
+                            className="form_input"
+                            type="text"
+                            id="city"
+                            placeholder="Enter city here"
+                            value={city}
+                            onChange={(e) => setCity(e.target.value)}
+                        />
+                    </div>
                 </div>
-                <div className="email">
-                    <label className="form_label" htmlFor="email">Email</label>
-                    <input
-                        required={false}
-                        type="email"
-                        id="email"
-                        className="form_input"
-                        placeholder="Enter email here"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
+                <div className="registration-footer">
+                    <button
+                        type="submit"
+                        className="company-btn">Register</button>
                 </div>
-                <div className="password">
-                    <label className="form_label" htmlFor="password">Password </label>
-                    <input
-                        required={true}
-                        className="form_input"
-                        type="password"
-                        id="password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                <div className="confirm-password">
-                    <label className="form_label" htmlFor="confirmPassword">Confirm Password </label>
-                    <input
-                        required={true}
-                        className="form_input"
-                        type="password"
-                        id="confirmPassword"
-                        placeholder="Confirm Password"
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                    />
-                </div>
-                <div className="phone">
-                    <label className="form_label" htmlFor="phone">Phone number</label>
-                    <input
-                        required={true}
-                        className="form_input"
-                        type="phone"
-                        id="phone"
-                        placeholder="Enter phone number"
-                        value={phone}
-                        onChange={(e) => setPhone(e.target.value)}
-                    />
-                </div>
-                <div className="city">
-                    <label className="form_label" htmlFor="city">City</label>
-                    <input
-                        required={true}
-                        className="form_input"
-                        type="text"
-                        id="city"
-                        placeholder="Enter city here"
-                        value={city}
-                        onChange={(e) => setCity(e.target.value)}
-                    />
-                </div>
-            </div>
-            <div className="registration-footer">
-                <button
-                    type="submit"
-                    className="btn">Register</button>
-            </div>
-        </form>
+            </form>
+        </div>
     )
 }
 
