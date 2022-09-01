3a3fe3760a4f5bc581d5b28412906bf223545a58
szgyuszi <szgybence@gmail.com>
2022-09-01 15:17:39 +0200

    fix: redesigned form

--- lnkdoutfrontend/src/pages/Student/components/Registration.js
+++ lnkdoutfrontend/src/pages/Student/components/Registration.js
@@ -3,6 +3,11 @@ import React from 'react';
 function Registration({name, setName, age, setAge, email, setEmail, phone, setPhone, password, setPassword,
                               confirmPassword, setConfirmPassword, city, setCity, handleSubmit}) {
     return (
+        
+        <>
+            <div className={"welcome-register"}>
+                <h1>{"Register as Student"}</h1>
+            </div>
         <div className={'registration'}>
             <img src={'https://images.unsplash.com/photo-1548142813-c348350df52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVvcGxlJTIwc21pbGV8ZW58MHx8MHx8&w=1000&q=80'}/>
             <div className={'container'}>
@@ -76,6 +81,7 @@ function Registration({name, setName, age, setAge, email, setEmail, phone, setPh
                 </form>
             </div>
         </div>
+        </>
     );
 }
 
