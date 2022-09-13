563eeb69242cd7385cb441621a09612417c1d5af
SzPetra <petra.szilagyi27@gmail.com>
2022-09-13 11:24:43 +0200

    refactor: message model class

--- lnkdoutbackend/src/main/java/application/model/MessageModel.java
+++ lnkdoutbackend/src/main/java/application/model/MessageModel.java
@@ -1,28 +1,35 @@
 package application.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDate;
 import java.util.Random;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class MessageModel {
 
     private String subject;
     private String email;
     private String message;
     private LocalDate date;
+    @Id
+    @GeneratedValue
+    @JsonIgnore
     private int id;
+    @Column(unique = true)
+    private int publicKey;
 
-    public MessageModel(@JsonProperty("subject") String subject, @JsonProperty("fromPerson") String email,
-                        @JsonProperty("message") String message) {
-        this.subject = subject;
-        this.email = email;
-        this.message = message;
-        this.date = LocalDate.now();
-        this.id = new Random().nextInt(1, 100);
-    }
-
-    public int getId() {
-        return id;
-    }
 }
