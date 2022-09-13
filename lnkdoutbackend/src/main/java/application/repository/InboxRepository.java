cf13b68d8bb36c8dff63444ff65b297e2091340b
SzPetra <petra.szilagyi27@gmail.com>
2022-09-13 11:22:19 +0200

    feat: add inbox repository

---
+++ lnkdoutbackend/src/main/java/application/repository/InboxRepository.java
@@ -0,0 +1,7 @@
+package application.repository;
+
+import application.model.MessageModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InboxRepository extends JpaRepository<MessageModel, Integer> {
+}
