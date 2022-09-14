47db6e797ed956e9a21d9f571466de21642bf21c
szgyuszi <szgybence@gmail.com>
2022-09-14 10:57:41 +0200

    feat: implement test for student repository

--- lnkdoutbackend/src/test/java/application/repository/StudentRepositoryTest.java
+++ lnkdoutbackend/src/test/java/application/repository/StudentRepositoryTest.java
@@ -1,7 +1,27 @@
 package application.repository;
 
+import application.model.StudentModel;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class StudentRepositoryTest {
+
+    @Autowired StudentRepository studentRepository;
+
+    @BeforeEach
+    void setup() {
+        StudentModel testModel = StudentModel.builder().name("Andro").city("Budapest").build();
+        studentRepository.saveAndFlush(testModel);
+    }
+
+    @Test
+    void isStudentExistByName() {
+        assertTrue(studentRepository.existsByName("Andro"));
+    }
 
 }
