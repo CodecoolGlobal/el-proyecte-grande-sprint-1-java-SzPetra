3cd53af823e0e446ca78f09c515e052e618681c4
szgyuszi <szgybence@gmail.com>
2022-09-14 09:28:17 +0200

    feat: setup test files

---
+++ lnkdoutbackend/src/test/java/application/service/CompanyServiceTest.java
@@ -0,0 +1,20 @@
+package application.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompanyServiceTest {
+
+    @Test
+    void registerCompany() {
+    }
+
+    @Test
+    void getAllCompany() {
+    }
+
+    @Test
+    void getCompanyById() {
+    }
+}
