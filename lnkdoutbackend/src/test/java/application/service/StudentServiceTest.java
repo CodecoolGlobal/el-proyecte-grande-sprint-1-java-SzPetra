36d19734b2771fc9b8f06b63aa13ca55fad8ac2f
szgyuszi <szgybence@gmail.com>
2022-09-14 10:51:57 +0200

    feat: implement test for company repository

---
+++ lnkdoutbackend/src/test/java/application/service/StudentServiceTest.java
@@ -0,0 +1,23 @@
+package application.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentServiceTest {
+
+
+    @Test
+    void getAllStudent() {
+
+
+    }
+
+    @Test
+    void registerStudent() {
+    }
+
+    @Test
+    void getStudentById() {
+    }
+}
