07d9f76b232899f3312e21e3ad25c225059bcfb1
szgyuszi <szgybence@gmail.com>
2022-09-14 10:51:43 +0200

    feat: implement test for company repository

--- lnkdoutbackend/src/main/java/application/service/CompanyService.java
+++ lnkdoutbackend/src/main/java/application/service/CompanyService.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Service
 public class CompanyService {
 
-   private CompanyRepository companyRepository;
+   private final CompanyRepository companyRepository;
 
    @Autowired
     public CompanyService(CompanyRepository companyRepository) {
