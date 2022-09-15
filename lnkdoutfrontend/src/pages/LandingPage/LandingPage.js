33e33f480d75bc518f39c5a5d49ca294a130bc27
SzPetra <petra.szilagyi27@gmail.com>
2022-09-15 11:53:52 +0200

    feat: add list all clients buttons to header

--- lnkdoutfrontend/src/pages/LandingPage/LandingPage.js
+++ lnkdoutfrontend/src/pages/LandingPage/LandingPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Content from "./components/Content";
-// import Footer from "../../components/Footer";
 import GetStarted from "./components/GetStarted";
 import About from "./components/About";
 import Contact from "./components/Contact";
