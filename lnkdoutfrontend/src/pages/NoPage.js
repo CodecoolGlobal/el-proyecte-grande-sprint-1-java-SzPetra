251feb728be6749d7e50f2287a1e9c74b22f7170
szgyuszi <szgybence@gmail.com>
2022-09-15 11:01:04 +0200

    feat: add design to 404 page

--- lnkdoutfrontend/src/pages/NoPage.js
+++ lnkdoutfrontend/src/pages/NoPage.js
@@ -8,8 +8,7 @@ function NoPage() {
             <div className="container">
 
                 <h1 className="text-center ">404</h1>
-                <div className="four_zero_four_bg">
-                </div>
+                <div className="four_zero_four_bg"></div>
 
                 <div className="contant_box_404">
                     <h3 className="h2">Look like you're lost</h3>
