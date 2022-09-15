2d8bf0bd1b044a763a1e76809cc0e3d96ac36376
szgyuszi <szgybence@gmail.com>
2022-09-15 15:38:41 +0200

    add: merging manually

---
+++ lnkdoutbackend/src/main/java/application/service/InboxService.java
@@ -0,0 +1,32 @@
+package application.service;
+
+import application.model.MessageModel;
+import application.model.StudentModel;
+import application.repository.InboxRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class InboxService {
+
+    private InboxRepository inboxRepository;
+
+    @Autowired
+    public InboxService(InboxRepository inboxRepository) {
+        this.inboxRepository = inboxRepository;
+    }
+
+    public List<MessageModel> getAllMessages() {
+        return inboxRepository.findAll();
+    }
+
+    public void sendMessage(MessageModel message) {
+        inboxRepository.save(message);
+    }
+
+    public List<MessageModel> getMessagesForProfile(StudentModel student) {
+        return inboxRepository.findMessageModelsByStudent(student);
+    }
+
+}
---
+++ lnkdoutfrontend/src/pages/inbox/components/SendMessageForm.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import '../assets/messageForm.css';
+import {Link} from "react-router-dom";
+import {FaHandPointLeft} from "react-icons/fa";
+
+function SendMessageForm({subject, setSubject, email, setEmail, message, setMessage, sendMessage, id}) {
+    return (<div className={'inbox-container'}>
+
+            <section className={'inbox-form-container'}>
+                <form className={'inbox-form'} onSubmit={sendMessage}>
+
+                    <label htmlFor={'subject'}>Subject: </label>
+                    <input
+                        id={'subject'}
+                        value={subject}
+                        autoFocus={true}
+                        placeholder={"'Next week's interview'"}
+                        onChange={(e) => setSubject(e.target.value)}
+                    />
+
+                    <label htmlFor={'from'}>From: </label>
+                    <input
+                        id={'from'}
+                        value={email}
+                        required={true}
+                        placeholder={'mike.wheeler@strthings.com'}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+
+                    <label htmlFor={'message'}>Message: </label>
+                    <textarea
+                        id={'message'}
+                        value={message}
+                        required={true}
+                        placeholder={'we found another hole..it leads to the upside-down'}
+                        onChange={(e) => setMessage(e.target.value)}
+                    ></textarea>
+
+                    <button id={'send-message-btn'} type={'submit'}>Send</button>
+                </form>
+            </section>
+            <Link className={'back-link'} to={`/student/profile/${id}`}>
+                <FaHandPointLeft className={'hand-icon'}/>
+                Back
+            </Link>
+        </div>);
+}
+
+export default SendMessageForm;
