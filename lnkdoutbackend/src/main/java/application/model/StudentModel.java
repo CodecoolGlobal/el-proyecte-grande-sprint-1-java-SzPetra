e54fee2036698da0240e85ad8e06e9590694bc4a
enwoy <androkityb@gmail.com>
2022-09-28 13:31:19 +0200

    bugfix: change overrode method returns

--- lnkdoutbackend/src/main/java/application/model/StudentModel.java
+++ lnkdoutbackend/src/main/java/application/model/StudentModel.java
@@ -34,26 +34,26 @@ public class StudentModel extends BaseModel{
 
     @Override
     public String getUsername() {
-        return null;
+        return name;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
