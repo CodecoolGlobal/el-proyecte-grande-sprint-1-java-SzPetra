f36746f9c50208071b98ba3fd4460e5acf37f79c
szgyuszi <szgybence@gmail.com>
2022-09-29 09:52:54 +0200

    refactor: delete unused import

--- lnkdoutbackend/src/main/java/application/controller/student/StudentController.java
+++ lnkdoutbackend/src/main/java/application/controller/student/StudentController.java
@@ -5,7 +5,6 @@ import application.model.StudentModel;
 import application.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
