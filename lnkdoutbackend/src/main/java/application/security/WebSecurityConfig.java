832b7190a7fb5d2a8a8711b9622af003341bc451
szgyuszi <szgybence@gmail.com>
2022-09-29 09:36:07 +0200

    feat: add permit all in student post route to register freely

--- lnkdoutbackend/src/main/java/application/security/WebSecurityConfig.java
+++ lnkdoutbackend/src/main/java/application/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package application.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -42,6 +43,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.POST, "/student").permitAll()
                 .anyRequest()
                 .authenticated();
     }
