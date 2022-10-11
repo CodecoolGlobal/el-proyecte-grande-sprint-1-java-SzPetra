4913df7af154c86cf88687f747b999a7cd987407
szgyuszi <szgybence@gmail.com>
2022-10-11 11:01:55 +0200

    feat: add filtered helper method

--- lnkdoutbackend/src/main/java/application/service/JobService.java
+++ lnkdoutbackend/src/main/java/application/service/JobService.java
@@ -1,10 +1,12 @@
 package application.service;
 
+import application.model.CompanyModel;
 import application.model.JobModel;
 import application.repository.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,10 +20,30 @@ public class JobService {
     }
 
     public List<JobModel> getAllJobs() {
-        return jobRepository.findAll();
+        List<JobModel> filteredJobs = new ArrayList<>();
+        List<JobModel> allJobs = jobRepository.findAll();
+        for (JobModel jobModel : allJobs) {
+            filteredJobs.add(getFilteredJob(jobModel));
+        }
+        return filteredJobs;
     }
 
     public JobModel getJobById(int id) {
-        return jobRepository.getById(id);
+        JobModel byId = jobRepository.getById(id);
+        return getFilteredJob(byId);
+
+    }
+
+
+    private JobModel getFilteredJob(JobModel jobModel) {
+        CompanyModel company = jobModel.getCompany();
+        company.setCity(null);
+        company.setFavoriteStudents(null);
+        company.setEmail(null);
+        company.setPassword(null);
+        company.setPhone(null);
+        company.setRoles(null);
+        jobModel.setCompany(company);
+        return jobModel;
     }
 }
